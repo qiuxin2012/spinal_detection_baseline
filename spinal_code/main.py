a69d49e35d664713bd067c4859faab703bebb356
qiuxin2012 <qiuxin2012cs@gmail.com>
2020-08-13 19:44:22 -0400

    add evaluate

--- spinal_code/main.py
+++ spinal_code/main.py
@@ -34,9 +34,7 @@ if __name__ == '__main__':
 
     def train_dataloader():
         train_studies, train_annotation, train_counter = construct_studies(
-            '/home/cpx/qiuxin/ali/spinal_detection_baseline/data/lumbar_train150', '/home/cpx/qiuxin/ali/spinal_detection_baseline/data/lumbar_train150_annotation.json', multiprocessing=False)
-        valid_studies, valid_annotation, valid_counter = construct_studies(
-            '/home/cpx/qiuxin/ali/spinal_detection_baseline/data/lumbar_train51/', '/home/cpx/qiuxin/ali/spinal_detection_baseline/data/lumbar_train51_annotation.json', multiprocessing=False)
+            'data/lumbar_train150', 'data/lumbar_train150_annotation.json', multiprocessing=False)
 
         train_images = {}
         for study_uid, study in train_studies.items():
@@ -45,7 +43,7 @@ if __name__ == '__main__':
         train_dataset = DisDataSet(studies=train_studies, annotations=train_annotation, sagittal_size=dis_model.sagittal_size,
                                    transverse_size=dis_model.sagittal_size, k_nearest=0, prob_rotate=1,
                                    max_angel=180, num_rep=10, max_dist=8)
-        return DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=3,
+        return DataLoader(train_dataset, batch_size=8, shuffle=True, num_workers=3,
                           pin_memory=False, collate_fn=train_dataset.collate_fn)
 
     az_model = TorchModel.from_pytorch(dis_model)
@@ -54,11 +52,20 @@ if __name__ == '__main__':
     train_featureset = FeatureSet.pytorch_dataloader(train_dataloader, "", "")
     zooOptimizer = Adam(lr=1e-5)
     estimator = Estimator(az_model, optim_methods=zooOptimizer)
-    estimator.train_minibatch(train_featureset, zoo_loss, end_trigger=MaxEpoch(20),
+    estimator.train_minibatch(train_featureset, zoo_loss, end_trigger=MaxEpoch(1),
                               checkpoint_trigger=EveryEpoch())
 
+    valid_studies, valid_annotation, valid_counter = construct_studies(
+            'data/train/', 'data/lumbar_train51_annotation.json', multiprocessing=False)
+    valid_evaluator = Evaluator(
+        dis_model, valid_studies, 'data/lumbar_train51_annotation.json', num_rep=20, max_dist=6,
+    )
+    metrics_values = valid_evaluator(az_model.to_pytorch(), None, valid_evaluator.metric)
+    for a, b in metrics_values:
+        print('valid {}: {}'.format(a, b))
+
     # 预测
-    testA_studies = construct_studies('/home/cpx/qiuxin/ali/spinal_detection_baseline/data/lumbar_testA50/', multiprocessing=False)
+    testA_studies = construct_studies('data/lumbar_testA50/', multiprocessing=False)
 
     result = []
     for study in testA_studies.values():
